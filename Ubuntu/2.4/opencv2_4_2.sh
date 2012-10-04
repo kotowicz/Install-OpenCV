b5d6c9f75d43a0554da9d8cbfe2225b5363bc8b4
Andreas Kotowicz <andreas.kotowicz@gmail.com>
2012-10-04 11:08:24 +0200

    fix ld config issue.

--- Ubuntu/2.4/opencv2_4_2.sh
+++ Ubuntu/2.4/opencv2_4_2.sh
@@ -61,6 +61,6 @@ cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make
 sudo make install
-sudo echo “/usr/local/lib” >> /etc/ld.so.conf
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
 echo "OpenCV 2.4.2 ready to be used"
