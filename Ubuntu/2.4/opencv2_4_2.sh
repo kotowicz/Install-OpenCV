442ce5a5260586e7c8f737e889d38e5558c5b6ec
Andreas Kotowicz <andreas.kotowicz@gmail.com>
2012-10-04 10:49:59 +0200

    modified cmake statement.

--- Ubuntu/2.4/opencv2_4_2.sh
+++ Ubuntu/2.4/opencv2_4_2.sh
@@ -58,7 +58,7 @@ tar -xvf OpenCV-2.4.2.tar.bz2
 cd OpenCV-2.4.2
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make
 sudo make install
 sudo echo “/usr/local/lib” >> /etc/ld.so.conf
